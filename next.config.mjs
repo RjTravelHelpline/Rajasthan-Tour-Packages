9ba192756f27aad1e9e67cef21c39715017713fe
Yash Jangid <yashcx84@gmail.com>
2024-12-23 22:03:44 +0530

    Testing.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        unoptimized: true, // Disable image optimization globally
+    },
+    output: 'export'
 };
 
 export default nextConfig;
