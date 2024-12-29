0ae18507896d9dfb15a6333dc18638313d4d1044
Yash Jangid <yashcx84@gmail.com>
2024-12-29 21:23:53 +0530

    on the way

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.scss';
-import "../styles/components.scss";
-import "../styles/app.scss";
-import "../styles/routes.scss";
-import "../styles/blog.scss";
+import '../styles/components.scss';
+import '../styles/app.scss';
+import '../styles/routes.scss';
+import '../styles/blog.scss';
+import '../styles/fonts.scss';
 import GoogleAnalyticsClient from '@/components/GoogleAnalyticsClient';
 import NavbarSwitcher from '@/components/NavBarSwitcher';
 import FooterSwitcher from '@/components/FooterSwitcher';
@@ -32,16 +33,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="PU85YcSvNVLnD66cxW67fVE1Xig2NKBngonkdHmyb5s" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-        <noscript>
-          {/* Fallback if JavaScript is disabled */}
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Lobster&display=swap"
-          />
-        </noscript>
       </head>
       <body>
         <GoogleAnalyticsClient />
--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
 };
 
-export default bundleAnalyzer(nextConfig);
+export default nextConfig;
