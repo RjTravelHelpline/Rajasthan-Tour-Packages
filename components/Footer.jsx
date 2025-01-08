a1532c17a46134ab79c9835c19c2127717e29776
RjTravelHelpline <yj606089@gmail.com>
2025-01-08 12:48:48 +0530

    footer finalized with necessary components attachments.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -15,12 +15,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Col, Container, Modal, Row } from 'react-bootstrap';
 import ContactForm from './ContactForm';
-import { BiChevronRight, BiChevronDown, BiChevronR, BiChevronDownight, BiLogoTripAdvisor } from 'react-icons/bi';
-import { PiFacebookLogoFill, PiInstagramLogoFill, PiSkypeLogoFill } from 'react-icons/pi';
+import { BiChevronRight, BiChevronDown, BiChevronR, BiChevronDownight, BiLogoTripAdvisor, BiPhoneCall, BiLinkAlt, BiSolidPhoneCall } from 'react-icons/bi';
+import { PiBrandyFill, PiFacebookLogoFill, PiInstagramLogoFill, PiSkypeLogoFill } from 'react-icons/pi';
 import { RiCustomerService2Line, RiLink, RiWhatsappFill } from 'react-icons/ri';
 import { MdContactSupport, MdMail } from 'react-icons/md';
 import { BsArrowUpRightCircleFill, BsChevronDoubleRight, BsChevronRight } from 'react-icons/bs';
 import { FaLocationArrow } from 'react-icons/fa';
+import { AiFillSafetyCertificate } from 'react-icons/ai';
+import { CgInsights } from "react-icons/cg";
 
 const Footer = () => {
     const tripAdvisorLogo = '/Images/Logos/tripadvisor-logo.webp';
@@ -359,70 +361,14 @@ const Footer = () => {
                                     </a>
                                 </div>
                             </div>
-                        </Row>
-                        <Row className="d-flex justify-content-center align-items-center">
-                            <div className="col-12 d-flex flex-column footer-links justify-content-center align-items-center">
-                                <div className="footer-logo w-100 d-flex justify-content-center align-items-center">
-                                    <a
-                                        href="https://rajasthantourpackages.in/"
-                                        target="_blank"
-                                        className="d-flex justify-content-center align-items-center image-link"
-                                        aria-label='rajasthan travel helpline'
-                                    >
-                                        <Image width={600} height={300} src={footerLogo} alt="Rajasthan Travel Helpline" title='Rajasthan Travel Helpline Contact Details' />
-                                    </a>
-                                </div>
-                                <div className="social-icons w-100 d-flex justify-content-center align-items-center gap-4 my-4">
-                                    <a
-                                        href="https://www.instagram.com/rajasthantravelhelpline/"
-                                        className="social-icon"
-                                        target="_blank"
-                                        aria-label='go to social'
-                                    >
-                                        <FaInstagram />
-                                    </a>
-                                    <a
-                                        href="https://www.facebook.com/rajasthantravelhelpline/"
-                                        className="social-icon"
-                                        target="_blank"
-                                        aria-label='go to social'
-                                    >
-                                        <FaFacebook />
-                                    </a>
-                                    <a
-                                        href="mailto:mail@rajasthantravelhelpline.com"
-                                        className="social-icon"
-                                        target="_blank"
-                                        aria-label='go to social'
-                                    >
-                                        <IoIosMail />
-                                    </a>
-                                    <a
-                                        href="https://www.tripadvisor.in/Attraction_Review-g304555-d24123312-Reviews-Rajasthan_Travel_Helpline-Jaipur_Jaipur_District_Rajasthan.html"
-                                        className="social-icon"
-                                        target="_blank"
-                                        aria-label='go to social'
-                                    >
-                                        <FaTripadvisor />
-                                    </a>
-                                </div>
-                            </div>
-                        </Row>
-                        <Row className="footer-end">
-                            <p className="copyright fw-normal text-center my-4 text-capitalize" style={{ color: '#898989' }}>
-                                © 2003-{mydate} | Rajasthan travel helpline | All Rights
-                                Reserved.
-                            </p>
-                        </Row>
+                        </Row>  
                     </div>
                 </div>
             </section> */}
-
-
             <div className="container-fluid bg-black py-5" style={{ borderTop: '1px solid var(--color-gray)' }}>
                 <div className="container">
-                    <div className="row d-flex justify-content-start aiign-items-center footer-links py-4">
-                        <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
+                    <div className="row d-flex justify-content-start aiign-items-center footer-links ">
+                        <div className="col-lg-3 col-md-6 col-sm-12 mb-4 d-flex flex-lg-column flex-sm-row justify-content-between align-items-start gap-2">
                             <div className="footer-logo d-flex flex-column justify-content-start align-items-start gap-2">
                                 <a
                                     href="https://rajasthantourpackages.in/"
@@ -437,7 +383,7 @@ const Footer = () => {
                                     <p className='text-capitalize text-white web-title fw-bold mb-0'>Rajasthan Travel Helpline</p>
                                 </div>
                             </div>
-                            <div className="footer-icon d-flex justify-content-start align-items-center gap-3 mt-3">
+                            <div className="footer-icon d-flex justify-content-start align-items-center gap-3">
                                 <a href="https://www.facebook.com/rajasthantravelhelpline/" className='fs-3 rounded-5 d-flex justify-content-center align-items-center text-white opacity-50 anchor-link-i'>
                                     <PiFacebookLogoFill />
                                 </a>
@@ -453,8 +399,12 @@ const Footer = () => {
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-                            <p className='text-capitalize text-white web-title fw-bold'>useful links</p>
-                            <hr />
+                            <BiLinkAlt className='color-tertary opacity-75 fs-4 mb-1' />
+                            <hr className='w-25' />
+                            <p className='text-capitalize text-white web-title fw-bold d-flex align-items-center'>
+                                useful links
+                            </p>
+                            {/* <hr /> */}
                             <div className='d-flex justify-content-center flex-column align-items-start gap-2'>
                                 {usefulLinks.map((link) => (
                                     <Link
@@ -469,8 +419,12 @@ const Footer = () => {
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-                            <p className='text-capitalize text-white web-title fw-bold'>company insights</p>
-                            <hr />
+                            <CgInsights className='color-tertary opacity-75 fs-4 mb-1' />
+                            <hr className='w-25' />
+                            <p className='text-capitalize text-white web-title fw-bold d-flex align-items-center'>
+                                company insights
+                            </p>
+                            {/* <hr /> */}
                             <div className='d-flex justify-content-center flex-column align-items-start gap-2'>
                                 {companyLinks.map((link) => (
                                     <Link
@@ -485,24 +439,95 @@ const Footer = () => {
                             </div>
                         </div>
                         <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
-                            <p className='text-capitalize text-white web-title fw-bold'>get in touch</p>
-                            <hr />
-                            <div className='mt-2 d-flex flex-column align-items-start'>
-                                <a href="tel:+91-9024337038" className='text-lowercase d-flex justify-content-center align-items-center anchor-link-u'>
-                                    <span className='fs-4 me-2 color-tertary'><RiCustomerService2Line /></span>
-                                    <span className='text-white opacity-50 fw-normal'>(+91)-9024337038</span>
+                            <AiFillSafetyCertificate className='color-tertary opacity-75 fs-4 mb-1' />
+                            <hr className='w-25' />
+                            <p className='text-capitalize text-white web-title fw-bold d-flex align-items-center'>
+                                approved by
+                            </p>
+                            {/* <hr /> */}
+                            <div className='mt-2 d-flex gap-2 flex-wrap align-items-start'>
+                                <a
+                                    href="https://www.tourism.rajasthan.gov.in/"
+                                    className="overflow-hidden"
+                                    target="_blank"
+                                    aria-label='go to certification' style={{ backgroundColor: "#000", width: '6em' }}
+                                >
+                                    <Image width={600} height={300}
+                                        src={certified01}
+                                        alt="Rajasthan Tourism Certification"
+                                        title='Rajasthan Tourism Certification for Travel Services'
+                                        className="w-100 h-auto"
+                                        placeholder='empty'
+
+                                    />
                                 </a>
-                                <a href="https://wa.me/9166555888" target="_blank" className='text-lowercase d-flex justify-content-center align-items-center anchor-link-u'>
-                                    <span className='fs-4 me-2'><RiWhatsappFill style={{ color: '#4AB060' }} /></span>
-                                    <span className='text-white opacity-50 fw-normal'>(+91)-9024337038</span>
+                                <a
+                                    href="https://www.mca.gov.in/content/mca/global/en/home.html"
+                                    className="overflow-hidden"
+                                    target="_blank"
+                                    aria-label='go to certification' style={{ backgroundColor: "#000", width: '6em' }}
+                                >
+                                    <Image width={600} height={300}
+                                        src={certified02}
+                                        alt="Ministry of Corporate Affairs Certification"
+                                        title='Ministry of Corporate Affairs Certification'
+                                        className="w-100 h-auto"
+                                        placeholder='empty'
+
+                                    />
                                 </a>
-                                <a href="mailto:mail@rajasthantravelhelpline.com" className='text-lowercase d-flex justify-content-center align-items-center anchor-link-u'>
-                                    <span className='fs-4 me-2 anchor-link-u'><MdMail style={{ color: '#FF3C3C' }} /></span>
-                                    <span className='text-white opacity-50 fw-normal '>mail@rajasthantravelhelpline.com</span>
+                                <a
+                                    href="https://frto.org/"
+                                    className="overflow-hidden"
+                                    target="_blank"
+                                    aria-label='go to certification'
+                                    placeholder='empty' style={{ backgroundColor: "#000", width: '6em' }}
+                                >
+                                    <Image width={600} height={300}
+                                        src={certified04}
+                                        alt="FRTO Certification"
+                                        title='Obtaining of FRTO Certification for Travel Services'
+                                        className="w-100 h-auto"
+                                    />
                                 </a>
                             </div>
                         </div>
-                        <div className="col-12 mb-4">
+                        <div className="col-12 mt-5 pt-5 d-flex gap-3 flex-wrap align-items-center">
+                            <a href="tel:+91-9024337038" className='text-capitalize d-flex justify-content-center align-items-center '>
+                                <BiSolidPhoneCall className='color-tertary me-2 opacity-75 fs-4' />
+                                <span className='text-white opacity-75 me-2 text-capitalize web-title'>
+                                    Call:
+                                </span>
+                                <span className='text-capitalize text-white fw-light opacity-75 anchor-link-ul'>
+                                    (+91)-9024337038
+                                </span>
+                            </a>
+                            <span className='text-white opacity-50'>
+                                |
+                            </span>
+                            <a href="https://wa.me/9166555888" target="_blank" className='text-lowercase d-flex justify-content-center align-items-center'>
+                                <RiWhatsappFill className='color-tertary me-2 opacity-75 fs-4' />
+                                <span className='text-white opacity-75 me-2 text-capitalize web-title'>
+                                    Whatsapp:
+                                </span>
+                                <span className='text-white fw-light opacity-75 anchor-link-ul'>
+                                    (+91)-9166555888
+                                </span>
+                            </a>
+                            <span className='text-white opacity-50'>
+                                |
+                            </span>
+                            <a href="mailto:mail@rajasthantravelhelpline.com" target="_blank" className='text-lowercase d-flex justify-content-center align-items-center'>
+                                <MdMail className='color-tertary me-2 opacity-75 fs-4' />
+                                <span className='text-white opacity-75 me-2 text-capitalize web-title'>
+                                    Mail:
+                                </span>
+                                <span className='text-white fw-light opacity-75 anchor-link-ul'>
+                                    mail@rajasthantravelhelpline.com
+                                </span>
+                            </a>
+                        </div>
+                        <div className="col-12">
                             <hr />
                         </div>
                         <div className="col-lg-6 col-sm-12 mb-2">
@@ -510,15 +535,15 @@ const Footer = () => {
                         </div>
                         <div className="col-lg-6 col-sm-12 mb-2">
                             <div className="d-flex gap-2 justify-content-lg-end justify-content-sm-start align-items-center">
-                                <Link href="/privacy-policy" className='text-white opacity-25 fw-light font-min anchor-link-u-white'>
+                                <Link href="/privacy-policy" className='text-white opacity-50 fw-light font-min anchor-link-u-white'>
                                     privacy policy
                                 </Link>
                                 <span className='text-white opacity-25'>•</span>
-                                <Link href="/terms-and-conditions" className='text-white opacity-25 fw-light font-min anchor-link-u-white'>
+                                <Link href="/terms-and-conditions" className='text-white opacity-50 fw-light font-min anchor-link-u-white'>
                                     terms & conditions
                                 </Link>
                                 <span className='text-white opacity-25'>•</span>
-                                <Link href="/sitemap.xml" className='text-white opacity-25 fw-light font-min anchor-link-u-white'>
+                                <Link href="/sitemap.xml" className='text-white opacity-50 fw-light font-min anchor-link-u-white'>
                                     sitemap
                                 </Link>
                             </div>
@@ -526,6 +551,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
+            {/*  */}
 
             {/* Modal for Contact Form */}
             <Modal
