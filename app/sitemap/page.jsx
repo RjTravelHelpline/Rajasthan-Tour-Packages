3fc2c06b6e1aabc40019c5241b62475c13d393bc
RjTravelHelpline <yj606089@gmail.com>
2025-01-08 18:02:40 +0530

    structure built.

---
+++ app/sitemap/page.jsx
@@ -0,0 +1,125 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Page = () => {
+    return (
+        <>
+            <div className="container-fluid bg-black text-white">
+                <div className="container py-5">
+                    <div className="row">
+                        <div className="col-12 py-5">
+                            <h1 className='web-title fw-bold'>Sitemap</h1>
+                            <hr className="w-50" />
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
+                            <h4 className='web-title fw-light text-capitalize'>rajasthan tour by days</h4>
+                            <hr className='w-50' />
+                            <div>
+                                <ul>
+                                    <li>
+                                        <Link href='/02-04-days-rajasthan-tours' className='sitemap-link text-white opacity-50 web-title'>02-04-days-rajasthan-tours</Link>
+                                    </li>
+                                    <li>
+                                        <Link href='/02-05-days-rajasthan-tours' className='sitemap-link text-white opacity-50 web-title'>02-05-days-rajasthan-tours</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
+                            <h4 className='web-title fw-light text-capitalize'>rajasthan tour by cities</h4>
+                            <hr className='w-50' />
+                            <div>
+                                <ul>
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-chhattisgarh' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-chhattisgarh</Link>
+                                    </li>
+
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-delhi' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-delhi</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
+                            <h4 className='web-title fw-light text-capitalize'>rajasthan tour by themes</h4>
+                            <hr className='w-50' />
+                            <div>
+                                <ul>
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-chhattisgarh' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-chhattisgarh</Link>
+                                    </li>
+
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-delhi' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-delhi</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
+                            <h4 className='web-title fw-light text-capitalize'>exclusive tour packages</h4>
+                            <hr className='w-50' />
+                            <div>
+                                <ul>
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-chhattisgarh' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-chhattisgarh</Link>
+                                    </li>
+
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-delhi' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-delhi</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
+                            <h4 className='web-title fw-light text-capitalize'>rajasthan destinations</h4>
+                            <hr className='w-50' />
+                            <div>
+                                <ul>
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-chhattisgarh' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-chhattisgarh</Link>
+                                    </li>
+
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-delhi' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-delhi</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
+                            <h4 className='web-title fw-light text-capitalize'>cab rentals</h4>
+                            <hr className='w-50' />
+                            <div>
+                                <ul>
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-chhattisgarh' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-chhattisgarh</Link>
+                                    </li>
+
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-delhi' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-delhi</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
+                            <h4 className='web-title fw-light text-capitalize'>company</h4>
+                            <hr className='w-50' />
+                            <div>
+                                <ul>
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-chhattisgarh' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-chhattisgarh</Link>
+                                    </li>
+
+                                    <li>
+                                        <Link href='/rajasthan-tour-from-delhi' className='sitemap-link text-white opacity-50 web-title'>rajasthan-tour-from-delhi</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Page
