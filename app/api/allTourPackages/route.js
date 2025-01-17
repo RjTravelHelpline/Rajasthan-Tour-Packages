5e027baff11d7d19f34c9a23475135e8394f2860
RjTravelHelpline <yj606089@gmail.com>
2025-01-17 14:21:39 +0530

    API header caches added part 12

--- app/api/allTourPackages/route.js
+++ app/api/allTourPackages/route.js
@@ -1,12 +1,15 @@
 import { query } from '../utils/db';
 
-export async function GET(req) {
+export async function GET() {
   try {
     const rows = await query('SELECT * FROM all_tour_packages', []);
     return new Response(JSON.stringify(rows), {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
+        'Cache-Control': 'max-age=10',
+        'CDN-Cache-Control': 'max-age=60',
+        'Vercel-CDN-Cache-Control': 'max-age=60',
       },
     });
   } catch (error) {
