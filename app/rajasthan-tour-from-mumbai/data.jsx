27f32cf03df9cc2550334c6070649a30838dea0d
RjTravelHelpline <yj606089@gmail.com>
2025-01-19 15:06:47 +0530

    City tour updated.

--- app/rajasthan-tour-from-mumbai/data.jsx
+++ app/rajasthan-tour-from-mumbai/data.jsx
@@ -1,8 +1,8 @@
 import { Link } from "@/Utils/util";
 export const page_titles = [
     {
-        heading: '02 days rajasthan tour packages',
-        subheading: '01 night • 02 days',
+        heading: 'Rajasthan Tour From Mumbai',
+        subheading: '',
     },
 ];
 export const tour_overview = {
@@ -13,22 +13,14 @@ export const tour_overview = {
     ),
     content: (
         <>
-            Experience the charm of Rajasthan with our exclusive Rajasthan tour packages from Mumbai.
-            Whether you are planning a short trip to rajasthan or a longer vacation, we offer tailor-made Rajasthan
-            tours from Mumbai to suit your needs. Rajasthan, the land of kings, is known for its magnificent forts,
-            vibrant culture and desert landscapes. From the golden dunes of Jaisalmer to the breathtaking palaces of
-            Jaipur and the serene lakes of Udaipur, every corner of Rajasthan tells a unique story. Our tour packages
-            from Mumbai to Rajasthan include visits to famous destinations like Jodhpur, Pushkar and Mount Abu.
+            Experience the charm of Rajasthan with our select <strong>Rajasthan tour packages from Mumbai</strong>. We have Rajasthan tours from Mumbai according to different time durations and purposes, whether you want to enjoy a short trip to Rajasthan or a long-term stay with Rajasthan travel packages. Rajasthan is the land of cultures, forts, and deserts and is about kings. Rajasthan has the {Link('golden dunes of Jaisalmer', 'https://www.rajasthantourpackages.in/jaisalmer')}, the breathtaking {Link('royal residences of Jaipur', 'https://www.rajasthantourpackages.in/jaipur')} and the peaceful {Link('lakes of Udaipur', 'https://www.rajasthantourpackages.in/udaipur')} Each corner of Rajasthan tells a one-of-a-kind story. Our tour packages from Mumbai to Rajasthan include trips to very famous destinations in Jodhpur, Pushkar, and {Link('Mount Abu', 'https://www.rajasthantourpackages.in/mount-abu')}.
+
             <br />
-            We take care of everything including comfortable transportation, handpicked hotels, guided sightseeing
-            and delicious meals. Traveling from Mumbai to Rajasthan has never been easier. Whether you prefer to
-            explore historical sites, enjoy cultural performances or experience the local cuisine, our packages are
-            designed to create unforgettable memories.
+            We take care of everything, counting comfortable transportation, handpicked hotels, guided touring and delicious dinners. Traveling from Mumbai to Rajasthan has never been easier. Whether you lean toward investigating historical sites, appreciating social exhibitions, or are involved in the nearby food, our packages are outlined to make extraordinary memories.
+
             <br />
-            Choose from our wide range of Rajasthan tour packages from Mumbai and embark on a journey full of
-            adventure, heritage and tradition. Book your Rajasthan tour from Mumbai today and experience the
-            magic of this royal state with hassle-free arrangements. Let us make your trip from Mumbai to Rajasthan
-            an unforgettable adventure.
+            From our various Rajasthan tour packages from Mumbai, you may choose your adventure, heritage, and tradition. Book your <strong>Rajasthan tour from Mumbai</strong> today and experience the magic of your royal tour to this state with completely hassle-free arrangements. Let us make memories of your trip from Mumbai to Rajasthan.
+
         </>
     )
 };
@@ -41,16 +33,12 @@ export const how_to_reach = {
     ),
     content: (
         <>
-            Planning a Rajasthan tour from Mumbai is easy with various travel options. Enjoy a scenic train journey
-            from Mumbai to Rajasthan, connecting cities like Jaipur, Udaipur, and Jodhpur, or opt for the
-            convenience of direct flights from Mumbai to Jaipur, Udaipur, or Jodhpur.
+            Planning a <strong>Rajasthan tour from Mumbai</strong> is easy with various travel options. It is easy to travel from Mumbai to Rajasthan through an enjoyable train journey from Mumbai to Jaipur, Udaipur or Jodhpur, or directly by flight to Jaipur, Udaipur or {Link('Jodhpur', 'https://www.rajasthantourpackages.in/jodhpur')}.
             <br />
-            Our Rajasthan Holiday Packages From Mumbai are crafted to make your dream trip a reality. Whether
-            you’re drawn to majestic forts, royal palaces, or the golden deserts, we have exclusive packages tailored
-            for you. Book now and unlock unforgettable memories with our budget-friendly Rajasthan travel
-            packages.
+            <strong>Rajasthan holiday packages</strong> from Mumbai are crafted to make your dream trip a reality. Whether you have a majestic fort with an added preference for royal palaces or golden sandy deserts, we have private packages just for you. Book now and open up extraordinary memories with our budget-friendly Rajasthan travel packages.
             <br />
-            Start planning your Rajasthan tour from Mumbai today and experience the magic of this royal destination.
+            Book your Rajasthan tour from Mumbai today, for this is one place you will love to visit for the royal destination.
+
         </>
     )
 }
@@ -59,47 +47,43 @@ export const tour_faqs = [
     {
         question: (
             <>
-                What is the best 3 days itinerary from Mumbai to Rajasthan?
+                What is the best 3-day itinerary from Mumbai to Rajasthan?
             </>
         ),
         answer: (
             <>
-                For a 3-day Rajasthan tour from Mumbai, explore Jaipur’s magnificent Amber Fort and Hawa Mahal.
-                Then, head to Udaipur to admire its serene lakes and the City Palace. Alternatively, visit Jodhpur for its
-                grand Mehrangarh Fort and explore the charm of the blue city. You can also immerse yourself in the rich
-                culture of Pushkar, which is famous for its vibrant camel fair and sacred lake. Explore a variety of 3-day
-                Rajasthan tour packages at www.rajasthantourpackages.in/03-days-rajasthan-tour-packages.
+                For a <strong>3-day Rajasthan tour from Mumbai</strong>, explore Jaipur’s magnificent Amber Fort and Hawa Mahal. Then, head to Udaipur to admire its serene lakes and the City Palace. Alternatively, visit Jodhpur for its grand Mehrangarh Fort and explore the charm of the blue city. You can also immerse yourself in the rich culture of Pushkar, which is famous for its vibrant camel fair and sacred lake. Explore a variety of {Link('3-day Rajasthan tour packages', 'http://www.rajasthantourpackages.in/03-days-rajasthan-tour-packages')}.
+
             </>
         )
     },
     {
         question: (
             <>
-                How can I book a Rajasthan Tour Package from Mumbai ?
+                How can I book a Rajasthan Tour Package from Mumbai?
+
             </>
         ),
         answer: (
             <>
-                To book a Rajasthan tour package from Mumbai, first decide on the destinations and duration of your trip.
-                Then, search for tour operators or travel agencies offering packages that suit your preferences. You can
-                compare prices, itineraries, and customer reviews. Once you find the right package, contact the agency
-                to confirm availability, make a booking, and arrange payment. Be sure to ask about inclusions like
-                transportation, accommodation, and meals to ensure everything is covered.
+                To book a <strong>Rajasthan tour package from Mumbai</strong>, first decide on the destinations and duration of your trip. Then, search for tour operators or travel agencies offering packages that suit your preferences. You can compare prices, itineraries, and customer reviews. Once you find the right package, contact the agency to confirm availability, make a booking, and arrange payment. Be sure to ask about inclusions like transportation, accommodation, and meals to ensure everything is covered.
+
+
             </>
         )
     },
     {
         question: (
             <>
-                What is the best time to visit Rajasthan ?
+                What is the best time to visit Rajasthan?
+
             </>
         ),
         answer: (
             <>
-                The best time to visit Rajasthan is from October to March, during the winter season. The weather is
-                pleasant and perfect for sightseeing and outdoor activities. This is also when Rajasthan hosts many
-                cultural festivals like the Jaipur Literature Festival and the Pushkar Camel Fair. Summers can be very
-                hot, so they are not ideal for travel, except for hill stations like Mount Abu.
+                The best time to visit Rajasthan is from <strong>October to March</strong> during the winter season. The weather is pleasant and perfect for sightseeing and outdoor activities. This is also when Rajasthan hosts many cultural festivals like the Jaipur Literature Festival and the {Link('Pushkar Camel Fair', 'https://www.rajasthantourpackages.in/blog/pushkar-camel-fair-cultural-tourism-rajasthan')}. Summers can be very hot, so they are not ideal for travel, except for hill stations like Mount Abu.
+
+
             </>
         )
     },
@@ -111,11 +95,8 @@ export const tour_faqs = [
         ),
         answer: (
             <>
-                Rajasthan offers various tour packages for travelers from Mumbai. These include visits to popular cities
-                like Jaipur, Udaipur, Jaisalmer, and Jodhpur. Packages often include sightseeing of forts, palaces,
-                deserts, and lakes. You can choose from luxury to budget-friendly Rajasthan tours from Mumbai based
-                on your preferences. Popular options include 5-day Golden Triangle Tours or 7-day Desert Adventures.
-                Check our website for affordable and customizable Rajasthan holiday packages from Mumbai.
+                Rajasthan offers different tour packages for travelers from Mumbai. They include city tours to places such as Jaipur, Udaipur, Jaisalmer and Jodhpur. Sightseeing normally forms part of packages and can comprise forts, palaces, deserts, and lakes. We have everything you prefer, from luxury to a cheap Rajasthan tour from Mumbai to suit your choice. Some of the famous trips are {Link('5-day Golden Triangle Tours', 'https://www.rajasthantourpackages.in/5days-golden-triangle-tour')} or <strong>7-day Desert Adventures</strong>. Check our website for affordable and customizable Rajasthan holiday packages from Mumbai.
+
 
             </>
         )
@@ -123,16 +104,15 @@ export const tour_faqs = [
     {
         question: (
             <>
-                What is the ideal Rajasthan travel itinerary for a week-long trip ?
+                What is the ideal Rajasthan travel itinerary for a week-long trip?
+
             </>
         ),
         answer: (
             <>
-                An ideal week-long Rajasthan travel itinerary includes Jaipur, Udaipur, and Jaisalmer. Start with Jaipur’s
-                forts and palaces, then head to Udaipur for its lakes and the City Palace. Conclude your trip in Jaisalmer
-                with a camel safari in the Thar Desert. You can also customize your itinerary to include Jodhpur or Mount
-                Abu. Our Rajasthan travel packages ensure you make the most of your time while staying within your
-                budget.
+                A perfect week-long <strong>Rajasthan travel itinerary</strong> includes Jaipur, Udaipur, and Jaisalmer. Jaipur offers forts and palaces, followed by the more famous lakes in Udaipur and the City Palace. End the tour in Jaisalmer by riding across the Thar desert on a camelback trip called a safari. You can customize your itinerary to incorporate Jodhpur or Mount Abu. Our Rajasthan travel packages ensure you make the most of your time while staying within your budget.
+
+
             </>
         )
     }
--- app/rajasthan-tour-from-mumbai/page.jsx
+++ app/rajasthan-tour-from-mumbai/page.jsx
@@ -1,76 +1,104 @@
-
+'use client'
 import Breadcrumb from "@/components/Breadcrumb";
-import Faq from "@/components/Faq";
-import ReadMoreToggle from "@/components/ReadMore";
-import TourCarousel from "@/components/TourCarousel";
+import HeroBanner from "@/components/HeroBanner";
+import ItineraryFaqs from "@/components/ItineraryFaqs";
 import TourPackages from "@/components/TourPackages";
-import { tourFromMumbai } from "@/data/CitiesData";
-import { mumbaiFaq } from "@/data/faqData";
-import { allStatesImages } from "@/data/imageData";
-import { Mumbai } from "@/data/tourByCitiesData";
+import { how_to_reach, page_titles, tour_faqs, tour_overview } from "./data";
+import { useEffect, useState } from "react";
 
-const RajasthanTourFromMumbai = () => {
-  const images = [
-    {
-      src: allStatesImages.mumbai.mumbaiBanner.src,
-      alt: allStatesImages.mumbai.mumbaiBanner.alt,
-      title: allStatesImages.mumbai.mumbaiBanner.title,
-    },
-  ];
+const Page = () => {
+  const [packages, setPackages] = useState([]);
 
-  const content = [
-    {
-      duration: '',
-      title: 'Rajasthan Tour From mumbai',
-    },
+  useEffect(() => {
+    const fetchPackages = async () => {
+      try {
+        const response = await fetch('/api/allTourPackages');
+        const data = await response.json();
+        setPackages(data);
+      } catch (error) {
+        console.error('Error fetching packages:', error);
+      }
+    };
+
+    fetchPackages();
+  }, []);
+  const filteredTitles = [
+    'Udaipur Mewar Tour',
+    'Golden Triangle Tour',
+    'Jaipur Ranthambore Weekend Tour',
+    'Jaipur - Jaisalmer Package Tour',
+    'Rajasthan Budget Tour',
+    'Rajasthan Honeymoon Tour'
   ];
+  const altered_packages = packages.filter((pkg) => filteredTitles.includes(pkg.title));
   return (
     <>
       {/* bread crumb */}
       <Breadcrumb breadcrumbKey="rajasthanTourFromMumbai" />
-      {/* banner */}
-      <div className="container-fluid home-banner days-banner-container destination-banner position-relative px-0">
-        <TourCarousel images={images} content={content} />
-      </div>
 
-      {/* data */}
-      <div className="container-fluid">
-        <div className="container py-5 px-0 position-relative">
-          <div className="row px-0">
-            <p
-              className="text-justify home-para px-3"
-              dangerouslySetInnerHTML={{ __html: tourFromMumbai.intro }}
-            />
-            <ReadMoreToggle className="text-justify home-para px-3" tag="div" contentArray={tourFromMumbai.showMoreContent} />
+      {/* banner */}
+      <HeroBanner backgroundImage='/Images/Banners/mumbai-banner.jpg' slides={page_titles} />
+
+      {/* overview */}
+      <div className="container-fluid px-0 mt-3">
+        <div className="container days-container overview">
+          <div className="row d-flex justify-content-center align-items-center days-overview px-2">
+            <div className="col-12 col-lg-11 col-sm-12 insider px-0">
+              <h3
+                className="text-capitalize px-3 mb-3 text-center fw-normal"
+              >{tour_overview.title}</h3>
+              <p
+                className="text-justify px-3 text-black"
+              >
+                {tour_overview.content}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* overview */}
+      <div className="container-fluid px-0 mt-3">
+        <div className="container days-container overview">
+          <div className="row d-flex justify-content-center align-items-center days-overview px-2">
+            <div className="col-12 col-lg-11 col-sm-12 insider px-0">
+              <h3
+                className="text-capitalize px-3 mb-3 text-center fw-normal text-black"
+              >{how_to_reach.title}</h3>
+              <p
+                className="text-justify px-3 text-black"
+              >
+                {how_to_reach.content}
+              </p>
+            </div>
           </div>
         </div>
       </div>
 
       {/* packages */}
-      <div className="container-fluid section-02 py-4">
-        <div className="container packages py-2">
-          <div className="row">
-            <h2
-              className="fw-normal text-capitalize"
-              dangerouslySetInnerHTML={{ __html: tourFromMumbai.package }}
-            ></h2>
-          </div>
-          <div className="row py-4 d-flex align-items-stretch px-2">
-            {Mumbai.slice(0, 3).map((pkg, index) => (
-              <TourPackages key={index} pkg={pkg} />
-            ))}
+      <div className="container-fluid px-0 my-3">
+        <div className="container overview">
+          <div className="row px-2 d-flex justify-content-center align-items-center package-more">
+            <div className="col-12 col-lg-11 col-sm-12 cost-table insider px-0 packages">
+              <h3 className="text-capitalize px-3 text-center fw-normal mb-2">
+                <span className="fw-bold">packages</span>
+              </h3>
+              <div className="py-2 d-flex align-items-stretch flex-wrap px-2">
+                {altered_packages.map((pkg, index) => (
+                  <TourPackages key={index} pkg={pkg} />
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-
       {/* faq's */}
       <div className="container-fluid px-0 section-03">
         <div className="container py-5">
           <div className="row">
-            <h3 className="mb-4">
+            <h3 className="mb-4 text-center">
               Frequently <span className="fw-normal">Asked Questions</span>
             </h3>
-            <Faq faqs={mumbaiFaq.questions} />
+            <ItineraryFaqs faqs={tour_faqs} />
           </div>
         </div>
       </div>
@@ -78,4 +106,4 @@ const RajasthanTourFromMumbai = () => {
   );
 };
 
-export default RajasthanTourFromMumbai;
+export default Page;
